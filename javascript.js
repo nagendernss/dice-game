8216276d89ee023861eafb51655f7affb31378bf
nagendernss <90512213+nagendernss@users.noreply.github.com>
2022-06-02 21:15:57 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,57 +3,56 @@
 
 // document.querySelectorAll("p")[0].innerText=name1;
 // document.querySelectorAll("p")[1].innerText=name2;
-
 var randomnumber1=Math.random();
 randomnumber1*=6;
 randomnumber1++;
 randomnumber1=Math.floor(randomnumber1);
 if(randomnumber1==1){
-    document.querySelector(".img1").setAttribute("src","dice1.png");
+    document.querySelector(".img1").setAttribute("src","images/dice1.png");
 }
 if(randomnumber1==2){
-    document.querySelector(".img1").setAttribute("src", "dice2.png");
+    document.querySelector(".img1").setAttribute("src", "images/dice2.png");
 }
 if(randomnumber1==3){
-    document.querySelector(".img1").setAttribute("src", "dice3.png");
+    document.querySelector(".img1").setAttribute("src", "images/dice3.png");
 }
 if(randomnumber1==4){
-    document.querySelector(".img1").setAttribute("src", "dice4.png");
+    document.querySelector(".img1").setAttribute("src", "images/dice4.png");
 }
 if(randomnumber1==5){
-    document.querySelector(".img1").setAttribute("src", "dice5.png");
+    document.querySelector(".img1").setAttribute("src", "images/dice5.png");
 }
 if(randomnumber1==6){
-    document.querySelector(".img1").setAttribute("src", "dice6.png");
+    document.querySelector(".img1").setAttribute("src", "images/dice6.png");
 }
 var randomnumber2=Math.random();
 randomnumber2*=6;
 randomnumber2++;
 randomnumber2=Math.floor(randomnumber2);
 if(randomnumber2==1){
-    document.querySelector(".img2").setAttribute("src","dice1.png");
+    document.querySelector(".img2").setAttribute("src","images/dice1.png");
 }
 if(randomnumber2==2){
-    document.querySelector(".img2").setAttribute("src", "dice2.png");
+    document.querySelector(".img2").setAttribute("src", "images/dice2.png");
 }
 if(randomnumber2==3){
-    document.querySelector(".img2").setAttribute("src", "dice3.png");
+    document.querySelector(".img2").setAttribute("src", "images/dice3.png");
 }
 if(randomnumber2==4){
-    document.querySelector(".img2").setAttribute("src", "dice4.png");
+    document.querySelector(".img2").setAttribute("src", "images/dice4.png");
 }
 if(randomnumber2==5){
-    document.querySelector(".img2").setAttribute("src", "dice5.png");
+    document.querySelector(".img2").setAttribute("src", "images/dice5.png");
 }
 if(randomnumber2==6){
-    document.querySelector(".img2").setAttribute("src", "dice6.png");
+    document.querySelector(".img2").setAttribute("src", "images/dice6.png");
 }
 
 if(randomnumber1>randomnumber2){
-    document.querySelector("h1").innerText="🚩 "+name1+ " Wins!";
+    document.querySelector("h1").innerText="🚩 Play 1 Wins!";
 }
 else if(randomnumber1<randomnumber2){
-    document.querySelector("h1").innerText=name2+" Wins! 🚩";
+    document.querySelector("h1").innerText="Play 2 Wins! 🚩";
 }
 else{
     document.querySelector("h1").innerText="🏁 Draw 🏁";
